2d9d0cf78f6975e30fd0c38cbb295c2d5252ec40
Rrahul Raja <rahulraja.ngp@gmail.com>
2022-08-23 12:02:24 +0530

    fix: fixed the error responses

--- __mocks__/api-service.js
+++ __mocks__/api-service.js
@@ -1,46 +1,67 @@
 export const signup = (username, password) => {
-    if(!username || !password) {
-        throw new Error('Username or Password required');
-    }
-
-    if(localStorage.getItem(username)) {
-        throw new Error('Username already exists!')
-    }
-
-    if(password.length < 8) {
-        throw new Error('Password should have alteast 8 characters.')
-    }
-
-    const pattern =  /^[A-Za-z]\w{7,14}$/
-    if(pattern.test(password)) {
-        throw new Error('Password must contain alphanumeric characters.');
-    }
-
-    localStorage.setItem(username, btoa(password));
-
+  if (!username || !password) {
     return {
-        success: true,
-        msg: 'User created successfully!'
-    }
-}
+      success: false,
+      msg: "Username or Password required!",
+    };
+  }
+
+  if (localStorage.getItem(username)) {
+    return {
+      success: false,
+      msg: "Username already exists!",
+    };
+  }
+
+  if (password.length < 8) {
+    return {
+      success: false,
+      msg: "Password should have alteast 8 characters.",
+    };
+  }
+
+  const pattern = /^[A-Za-z]\w{7,14}$/;
+  if (pattern.test(password)) {
+    return {
+      success: false,
+      msg: "Password must contain alphanumeric characters.",
+    };
+  }
+
+  localStorage.setItem(username, btoa(password));
+
+  return {
+    success: true,
+    msg: "User created successfully!",
+  };
+};
 
 export const signin = (username, password) => {
-    if(!username || !password) {
-        throw new Error('Username or Password required');
-    }
-
-    const storedPassword = localStorage.getItem(username);
-
-    if(!storedPassword) {
-        throw new Error('Username does not exists!')
-    }
-
-    if(storedPassword !== btoa(password)) {
-        throw new Error('Invalid credentials!');
-    }
-
+  if (!username || !password) {
     return {
-        success: true,
-        msg: 'Login successful!'
-    }
-}
+      success: false,
+      msg: "Username or Password required!",
+    };
+  }
+
+  const storedPassword = localStorage.getItem(username);
+
+  if (!storedPassword) {
+    return {
+      success: false,
+      msg: "Username does not exists!",
+    };
+  }
+
+  if (storedPassword !== btoa(password)) {
+    return {
+      success: false,
+      msg: "Invalid credentials!",
+    };
+  }
+
+  return {
+    success: true,
+    msg: "Login successful!",
+  };
+};
